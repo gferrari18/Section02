68b2b5a3a18c7d10c34eabe530c652e6023af110
gferrari18 <gferrari18@my.whitworth.edu>
2022-03-05 21:07:12 -0800

    nice

---
+++ meh.py
@@ -0,0 +1,29 @@
+
+#setting up variables which will but used to fill in equations. Random will be imported here
+import random
+A = random.randrange(1,100)
+B = random.randrange(1,100)
+C = random.randrange(1,100)
+
+#Tot will be the user's score. Each question with a correct answer will add 1 to Tot.
+#Sofware will keep track of user's score based on Tot
+Tot = 0
+
+equations = []
+
+
+
+equations = [x+2, x-2, x+8, x-8]
+for z in equations:
+    print(complex(z))
+    answer = int(input("Answer: "))
+    if x == answer:
+        continue
+
+
+
+
+
+
+
+
