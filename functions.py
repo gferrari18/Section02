2c35971dc52f51e9109f3f597904e3b20f70572e
gferrari18 <gferrari18@my.whitworth.edu>
2022-03-09 20:15:44 -0800

    nice

---
+++ functions.py
@@ -0,0 +1,33 @@
+""""
+def <- define the fuction
+name <- name of the function
+() - inside the parenthesis we place in PARAMETERS (variables, if you will)
+"""
+
+# PLACE PRINT STATEMENTS WHERE YOU CAN TO SEE WHAT IS GOING ON#
+
+#Have functions outside of the main file#
+
+"""
+def prod(n):
+    tot = 1
+    for x in range(1, n+1):
+        tot = tot * x
+    return tot
+
+n = int(input("Enter a number: "))
+prod = prod(n)
+
+print(prod)
+"""
+"""
+def alpha(text):
+    number = 0
+    for x in text:
+        if x.isalpha():
+            number = number + 1
+    return number
+
+text = input("enter a text:")
+print(alpha(text))
+"""
